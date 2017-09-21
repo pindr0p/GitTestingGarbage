a2be149bba48ede6ac38d783c88af45bd153e094
jdwestbrook86@gmail.com <jdwestbrook86@gmail.com>
2017-09-20 22:29:03 -0400

    Changed hi to hi 2

--- TestGitWithThis/Program.cs
+++ TestGitWithThis/Program.cs
@@ -10,7 +10,7 @@ namespace TestGitWithThis
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("HI");
+            Console.WriteLine("HI 2");
             var data = new List<int>(){ 1,2,6,7,3,4,5,6,7,3,5,53,354,534,534,534,534,534,534,
                 534,5,534,53,53,534,534,534,534,534,534,54,534,354,543,543,345,5,
                 34,534,354,534,354,5,43,354,4,45,5,34, 9};
